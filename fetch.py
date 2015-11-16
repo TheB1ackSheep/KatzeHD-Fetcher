42f7fa9364c033c1815ab7894e92f42e7b1c8918
Ubuntu <falook@falook-sea.falook-sea.i10.internal.cloudapp.net>
2015-11-16 12:55:39 +0000

    add get video function

--- fetch.py
+++ fetch.py
@@ -1,6 +1,8 @@
 import re
 import urllib2
 from bs4 import BeautifulSoup
+import sys
+import os
 
 '''link = raw_input('Enter movie link: ')
 name = raw_input('Name: ')
@@ -20,3 +22,43 @@ def check_resume_supported(url):
 	resp = urllib2.urlopen(req)
 	return resp.info().dict['content-length'] == '2'
 
+
+def get_video_links(name, url):
+	resp = urllib2.urlopen(url)
+	html = resp.read()
+	soup = BeautifulSoup(html, 'html.parser')
+	target = soup.find('div', attrs={'class', 'filmicerik'}).findChildren('center')
+	for raw in target:
+		html = unicode.join(u'\n',map(unicode,raw))
+		if 'docs.google' in html:
+			target = raw
+	target =  str(target).split('<br>')
+	for raw in target:
+		if 'docs.google' in raw:
+			if 'Soundtrack' in raw:
+				eng = raw
+			else:
+				th = raw
+	soup = BeautifulSoup(eng, 'html.parser')
+	files = []
+	for link in soup.find_all(href=re.compile('docs.google')):
+		link = BeautifulSoup(link.encode('utf-8'), 'html.parser')
+		f = dict()
+		f['name'] = name
+		f['resolution'] = link.text.strip()
+		f['url'] = link.a['href']
+		f['lang'] = 'EN'
+		files.append(f)
+	soup = BeautifulSoup(th, 'html.parser')
+	for link in soup.find_all(href=re.compile('docs.google')):
+                link = BeautifulSoup(link.encode('utf-8'), 'html.parser')
+                f = dict()
+                f['name'] = name
+                f['resolution'] = link.text.strip()
+                f['url'] = link.a['href']
+                f['lang'] = 'TH'
+                files.append(f)
+	return files
+#	print str(target[1]).split('<br>')
+
+get_video_links('', raw_input('Enter movie link: '))
