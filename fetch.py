c7ebcc3beab4e9bb6dc8ebc6fa25f0d4ddfb0c51
Ubuntu <falook@falook-sea.falook-sea.i10.internal.cloudapp.net>
2015-11-16 12:01:24 +0000

    add check resume support

--- fetch.py
+++ fetch.py
@@ -2,7 +2,7 @@ import re
 import urllib2
 from bs4 import BeautifulSoup
 
-link = raw_input('Enter movie link: ')
+'''link = raw_input('Enter movie link: ')
 name = raw_input('Name: ')
 response = urllib2.urlopen(link)
 html = response.read()
@@ -12,4 +12,11 @@ links = soup.find_all(href=re.compile('docs.google'))
 
 for link in links:
 	link2 = BeautifulSoup(link.encode('utf-8'), 'html.parser')
-	print ("nohup wget '{}' -O '{}.{}..mp4' & ").format(link2.a['href'], name, link2.text.strip())
+	print ("nohup wget '{}' -O '{}.{}..mp4' & ").format(link2.a['href'], name, link2.text.strip())'''
+
+def check_resume_supported(url):
+	req = urllib2.Request(url)
+	req.add_header('Range', 'bytes=0-1')
+	resp = urllib2.urlopen(req)
+	return resp.info().dict['content-length'] == '2'
+
